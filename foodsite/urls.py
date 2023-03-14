919d6fca6cdaef4dcd3641b274c8007c72c23857
Yar59 <75260156+Yar59@users.noreply.github.com>
2023-03-14 10:38:19 +0300

    add urls/views

--- foodsite/urls.py
+++ foodsite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from .views import show_main_page
+
 app_name = 'foodsite'
 
 urlpatterns = [
-
+    path('', show_main_page, name='index')
 ]
--- foodsite/views.py
+++ foodsite/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def show_main_page(request):
+    return render(request, template_name='index.html', context={})
