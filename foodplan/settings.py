17ff4735e47e7a0d1d445615647275802088dae8
nikita <bigmama93@mail.ru>
2023-03-21 22:21:49 +0300

    old setting

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -115,10 +115,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, env('STATIC_ROOT', 'static'))
+STATIC_ROOT = os.path.join(BASE_DIR, env('STATIC_ROOT', 'assets'))
 print(STATIC_ROOT)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
